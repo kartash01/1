a29a8e8ecf14f01cba761a75a3679373535c802e
kartash01 <92026071+kartash01@users.noreply.github.com>
2021-11-08 20:00:45 +0200

    загрузила пятую лабораторную

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from decimal import Decimal, getcontext
-from math import ceil, factorial
-
-n = int(input())
-num = n + 2
-
-def pi(num):
-    if num < 1:
-        print('Вы ввели недействительное число')
+def medians(nums1, nums2):
+    list_ = sorted(nums1 + nums2)
+    if len(list_) % 2 == 0:
+        m = ((list_[len(list_) // 2 - 1] + list_[len(list_) // 2]) / 2)       
+        return m
     else:
-        getcontext().prec = num
-        num_iter = ceil(num / 14)
-        constant = 426880 * Decimal(10005).sqrt()
-        exp = 1
-        numbr = 13591409
-        part = Decimal(numbr)
-        
-        for k in range(1, num_iter):
-            a = factorial(6 * k) // (factorial(3 * k) * factorial(k) ** 3)
-            numbr = numbr + 545140134
-            exp = exp * -262537412640768000
-            part += Decimal(a * numbr) / exp
-        return str(constant / part)[:-1]
-
-print(pi(num))
+        m = list_[len(list_) // 2]
+        return m
+    print('Объединенный список - {}.'.format(list_))
+    print('{} - медиана списка.'.format(list_))
